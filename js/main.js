0764cb712a844a0a84a54164dccd709114a65eb1
anami823 <anami823@gmail.com>
2019-07-26 09:45:39 -0700

    Inital commit with boiler plate files

---
+++ js/main.js
@@ -0,0 +1,17 @@
+/*----- constants -----*/ 
+/*----- app's state (variables) -----*/ 
+/*----- cached element references -----*/ 
+/*----- event listeners -----*/ 
+/*----- functions -----*/
+
+
+/* 
+1. Define constants 
+2. Define the app's state variables
+3. Create variables
+4. Add -delegated- Event listeners
+5. Invoke the init function
+6. Wait for use to trigger event
+7. Update state variables
+8. Invoke main render function
+*/
